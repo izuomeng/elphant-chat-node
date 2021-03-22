79217d59894cea8c34e077ddcc7cc87c19a8092c
daxiang.cyf <daxiang.cyf@alibaba-inc.com>
2021-03-22 11:17:12 +0800

    fix ctx.query类型问题

--- src/routes/friend.ts
+++ src/routes/friend.ts
@@ -4,16 +4,16 @@ import { getUserFriend, insertUserFriend } from '../service/user-friend';
 const router = new Router();
 
 router.get('/friend/apply', async (ctx) => {
-  ctx.body = await getUserFriend({ fromUid: ctx.query.uid });
+  ctx.body = await getUserFriend({ fromUid: String(ctx.query.uid) });
 });
 
 router.get('/friend/receive', async (ctx) => {
-  ctx.body = await getUserFriend({ toUid: ctx.query.uid });
+  ctx.body = await getUserFriend({ toUid: String(ctx.query.uid) });
 });
 
 router.get('/friend/now', async (ctx) => {
-  const receivedFriends = await getUserFriend({ toUid: ctx.query.uid, agreed: 'y' });
-  const appliedFriends = await getUserFriend({ fromUid: ctx.query.uid, agreed: 'y' });
+  const receivedFriends = await getUserFriend({ toUid: String(ctx.query.uid), agreed: 'y' });
+  const appliedFriends = await getUserFriend({ fromUid: String(ctx.query.uid), agreed: 'y' });
   ctx.body = [...receivedFriends, ...appliedFriends].sort((x, y) => y.time - x.time);
 });
 
